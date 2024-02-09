d69c3bdffc8e5eb6364b6312fd99f1b73fc0c4ba
Kerim Satirli <k.satirli@gmail.com>
2024-02-09 18:56:51 +0100

    updates `epd` references

--- baedge.py
+++ baedge.py
@@ -12,7 +12,7 @@ LIB_DIR = "./lib"
 
 # environment configuration
 font_face = os.getenv("BAEDGE_FONT_FACE", "./fonts/RobotoMono/regular.ttf")
-font_size = os.getenv("BAEDGE_FONT_SIZE", 15)
+font_size = os.getenv("BAEDGE_FONT_SIZE", "15")
 screen_model = os.getenv("BAEDGE_SCREEN_MODEL", "2in7")
 screen_revision = os.getenv("BAEDGE_SCREEN_REVISION", "_V2")
 log_level = os.getenv("LOG_LEVEL", "INFO")
@@ -22,21 +22,15 @@ logging.basicConfig(level=log_level)
 
 
 # conditionally import the correct library depending on env vartiables describing the EPD size
-#if os.path.exists(LIB_DIR):
-#    sys.path.append(LIB_DIR)
-
 epd_lib = importlib.import_module("lib.waveshare_epd.epd" + screen_model + screen_revision)
 logging.debug("[config] load EPD Library for Model %s (Rev: %s)",screen_model, screen_revision)
-
-# initialize the eInk screen
-#epd = epd_lib.EPD()
-
 
 ## TODO: Find a way to persist the epd config and not have to reset the screen on each request
 def init_screen():
     """ initialize eInk screen """
     try:
         epd = epd_lib.EPD()
+
         logging.debug("[init_screen] init screen")
         epd.init()
 
@@ -48,7 +42,7 @@ def init_screen():
         logging.exception(e)
 
 
-def clear_screen():
+def clear_screen(epd):
     """ clear content of eInk screen """
     logging.debug("[clear_screen]")
 
@@ -56,14 +50,14 @@ def clear_screen():
 #        init_screen()
 
         logging.debug("[clear_screen] sleep screen")
-        app.epd.sleep()
+        epd.sleep()
 
     except IOError as e:
         logging.error("[clear_screen] exception occurred")
         logging.exception(e)
 
 
-def write_text(text, style, epd):
+def write_text(epd, text, style):
     """ write textual content to eInk screen """
     logging.debug("[write_to_screen] text: %s", text)
     logging.debug("[write_to_screen] style: %s", style)
@@ -88,18 +82,19 @@ def write_text(text, style, epd):
         logging.exception(e)
 
 
-def write_image(image, epd):
+def write_image(epd, image):
     """ write image content to eInk screen """
     logging.debug("[write_image] image: %s", image)
+
+    # this is a placeholder for the image writing functionality
+    logging.debug(epd)
 
     # TODO: implement image writing
 
     return False
 
 
-def write_to_screen(text, image):
     """ write content to eInk screen """
-
     logging.debug("[write_to_screen] text: %s", text)
     logging.debug("[write_to_screen] image: %s", image)
 
@@ -113,22 +108,26 @@ def write_to_screen(text, image):
         font_config = ImageFont.truetype(font_face, font_size)
 
         # `255` clears the eInk screen
-        image = Image.new('1', (app.epd.height, app.epd.width), 255)
+        image = Image.new('1', (epd.height, epd.width), 255)
 
         draw = ImageDraw.Draw(image)
         draw.text((5, 60), text, font = font_config, fill = 0)
 
-        app.epd.display_Base(epd.getbuffer(image))
+        epd.display_Base(epd.getbuffer(image))
 
         logging.debug("[write_text] sleep screen")
-        app.epd.sleep()
+        epd.sleep()
+
+        return True
 
     except IOError as e:
         logging.error("[write_text] exception occurred")
         logging.exception(e)
 
+        return False
+
     except KeyboardInterrupt:
         logging.debug("[write_to_screen] keyboard interrupt")
 
-        epd_lib.epdconfig.module_exit()
+        epd.module_exit()
         sys.exit()
