038d1830c3d1b49310b8e7029207aa13f63b9f35
Kerim Satirli <k.satirli@gmail.com>
2024-02-10 20:48:16 +0100

    linting

--- baedge.py
+++ baedge.py
@@ -14,13 +14,13 @@ screen_model = os.getenv("BAEDGE_SCREEN_MODEL", "2in9b")
 screen_revision = os.getenv("BAEDGE_SCREEN_REVISION", "_v3")
 
 # (human) wearer configuration
-wearer_name = os.getenv("BAEDGE_WEARER_NAME", "{Ba,e}dge.")
-wearer_title = os.getenv("BAEDGE_WEARER_TITLE", "Orchestration at the Edge of Human and Compute")
+wearer_name = os.getenv("BAEDGE_WEARER_NAME", "{Ba,e}dge")
+wearer_title = os.getenv("BAEDGE_WEARER_TITLE", "Orchestration at the Edge of Human and Compute.")
 wearer_social = os.getenv("BAEDGE_WEARER_SOCIAL", "@wrklds")
 wearer_link = os.getenv("BAEDGE_WEARER_LINK", "https://workloads.io")
 
 # Nomad Environment configuration
-nomad_alloc_id = os.getenv("NOMAD_SHORT_ALLOC_ID", "MISSING")
+nomad_alloc_id = os.getenv("NOMAD_SHORT_ALLOC_ID", "n/a")
 nomad_addr_http = os.getenv("NOMAD_ADDR_http", "http://127.0.0.0")
 
 log_level = os.getenv("LOG_LEVEL", "INFO")
